350767e339c1e8c9e7d79b689c908d712888d191
Tuomas Pöyry <me@tuomas.id>
2025-02-18 16:25:07 +0200

    Remove quick launch

--- src/components/templates/utils.ts
+++ src/components/templates/utils.ts
@@ -4,30 +4,32 @@ export const launchTemplateWithTryOut = async (
   template: string,
   consoleUrl = CONSOLE_URL
 ) => {
-  const buttons = document.querySelectorAll(`[data-launch]`);
-  buttons.forEach((button) => button.setAttribute('disabled', 'true'));
+  window.location.href = consoleUrl;
 
-  const response = await fetch(
-    `${consoleUrl}/api/account/create-anonymous-trial`,
-    {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify({
-        template: template,
-        token: 'token', // TODO: ??
-      }),
-    }
-  );
+  // const buttons = document.querySelectorAll(`[data-launch]`);
+  // buttons.forEach((button) => button.setAttribute('disabled', 'true'));
 
-  const data = await response.json();
-  if (data.status === 'ok') {
-    window.location.href = `${consoleUrl}/launch/${data.token}`;
-  } else {
-    // FIXME: This is too ugly...
-    alert('Failed to start trial, please try again later...');
-    buttons.forEach((button) => button.removeAttribute('disabled'));
-  }
+  // const response = await fetch(
+  //   `${consoleUrl}/api/account/create-anonymous-trial`,
+  //   {
+  //     headers: {
+  //       Accept: 'application/json',
+  //       'Content-Type': 'application/json',
+  //     },
+  //     method: 'POST',
+  //     body: JSON.stringify({
+  //       template: template,
+  //       token: 'token', // TODO: ??
+  //     }),
+  //   }
+  // );
+
+  // const data = await response.json();
+  // if (data.status === 'ok') {
+  //   window.location.href = `${consoleUrl}/launch/${data.token}`;
+  // } else {
+  //   // FIXME: This is too ugly...
+  //   alert('Failed to start trial, please try again later...');
+  //   buttons.forEach((button) => button.removeAttribute('disabled'));
+  // }
 };
