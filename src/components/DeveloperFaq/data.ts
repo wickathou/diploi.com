c338a1dae66d252aa0cde653360a5666866b9d69
Tuomas Pöyry <me@tuomas.id>
2025-02-24 09:55:42 +0200

    Change wording on "switch service" FAQ

--- src/components/DeveloperFaq/data.ts
+++ src/components/DeveloperFaq/data.ts
@@ -4,13 +4,11 @@ export const developerFaqItems: TDeveloperFaqItem[] = [
   {
     id: 'developer-faq-1',
     question: 'Where does my code live?',
-    answer:
-      'Your code lives in your own GitHub repository. Diploi accesses it trough our GitHub app.',
+    answer: 'Your code lives in your own GitHub repository. Diploi accesses it trough our GitHub app.',
   },
   {
     id: 'developer-faq-3',
     question: 'What if I want to switch to another service?',
-    answer:
-      'Your code is yours to run anywhere. Powered by Kubernetes, a Diploi project runs wherever a Kubernetes cluster does.',
+    answer: 'Powered by Kubernetes, it is possible to run a Diploi project on any Kubernetes cluster.',
   },
 ];
