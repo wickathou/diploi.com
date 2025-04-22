14fc0391b038c622af7623139181378cefcd49d8
Tuomas Pöyry <me@tuomas.id>
2025-04-22 09:43:09 +0300

    Add Sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import sentry from '@sentry/astro';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
@@ -13,5 +14,15 @@ export default defineConfig({
     }),
     sitemap(),
     mdx(),
+    sentry({
+      dsn: 'https://df15d51df97371766b31299d9a5614a1@o4509195528437760.ingest.de.sentry.io/4509195529879632',
+      tracesSampleRate: 0,
+      replaysSessionSampleRate: 0,
+      replaysOnErrorSampleRate: 0,
+      sourceMapsUploadOptions: {
+        project: 'website',
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
   ],
 });
