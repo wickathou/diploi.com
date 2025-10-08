48b9bf495fc8c1ad478b44eabc579d4c9fa8895f
Tuomas <me@tuomas.id>
2025-10-08 10:15:07 +0300

    Disable localStorage trial tracking

--- src/components/StackBuilder/launchStack.ts
+++ src/components/StackBuilder/launchStack.ts
@@ -12,22 +12,23 @@ export const launchStack = async ({
   // Check if this user came from an AD campaing
   let utm: { [service: string]: string } | null = null;
   try {
-    if (window && window.localStorage) {
-      try {
-        utm = {
-          page: document.location.pathname,
-        };
+    utm = Object.fromEntries(new URLSearchParams(document.location.search));
+    // if (window && window.localStorage) {
+    //   try {
+    //     utm = {
+    //       page: document.location.pathname,
+    //     };
 
-        const { params, time } = JSON.parse(window.localStorage.getItem('utm') || '{}');
-        if (Date.now() - time < 24 * 60 * 60 * 1000) {
-          utm = Object.assign({}, utm, params);
-        }
-      } catch (error) {
-        console.error('Unable to parse UTM', error);
-      }
-    }
+    //     const { params, time } = JSON.parse(window.localStorage.getItem('utm') || '{}');
+    //     if (Date.now() - time < 24 * 60 * 60 * 1000) {
+    //       utm = Object.assign({}, utm, params);
+    //     }
+    //   } catch (error) {
+    //     console.error('Unable to parse UTM', error);
+    //   }
+    // }
   } catch (error) {
-    console.error('Failed to use localStorage', error);
+    console.error('Failed to parse utm', error);
   }
 
   const response = await fetch(`${apiUrl}/api/trpc/account.anonymousTrialCreate`, {
