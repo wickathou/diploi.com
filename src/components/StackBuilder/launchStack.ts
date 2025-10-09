39fa8e89a93681be240d56e9327bb6237757f744
Tuomas <me@tuomas.id>
2025-10-09 14:15:19 +0300

    Disable all telemetry (for now)

--- src/components/StackBuilder/launchStack.ts
+++ src/components/StackBuilder/launchStack.ts
@@ -12,13 +12,12 @@ export const launchStack = async ({
   // Check if this user came from an AD campaing
   let utm: { [service: string]: string } | null = null;
   try {
-    utm = Object.fromEntries(new URLSearchParams(document.location.search));
+    // utm = Object.fromEntries(new URLSearchParams(document.location.search));
     // if (window && window.localStorage) {
     //   try {
     //     utm = {
     //       page: document.location.pathname,
     //     };
-
     //     const { params, time } = JSON.parse(window.localStorage.getItem('utm') || '{}');
     //     if (Date.now() - time < 24 * 60 * 60 * 1000) {
     //       utm = Object.assign({}, utm, params);
